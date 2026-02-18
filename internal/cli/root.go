7e872356fa94dc3cbfe8ed743510a9fed3857e5c
Mario Cortés Ruiz <mario.cortes@meaningfuldata.eu>
2026-02-18 20:38:09 +0000

    feat: refactor download functionality into downloader package

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -3,8 +3,8 @@ package cli
 import (
 	"fmt"
 	"os"
-	"os/exec"
 
+	"github.com/ruizmaa/musicfreeier/pkg/downloader"
 	"github.com/spf13/cobra"
 )
 
@@ -15,7 +15,7 @@ var rootCmd = &cobra.Command{
 	SilenceUsage: true, // don't print usage on runtime errors
 	RunE: func(cmd *cobra.Command, args []string) error {
 		url := args[0]
-		return download(url)
+		return downloader.Download(url)
 	},
 }
 
@@ -24,30 +24,4 @@ func Execute() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-}
-
-func download(url string) error {
-	fmt.Printf("⬇  Downloading: %s\n", url)
-
-	ytdlp, err := exec.LookPath("yt-dlp")
-	if err != nil {
-		return fmt.Errorf("yt-dlp not found in PATH: %w", err)
-	}
-
-	cmd := exec.Command(ytdlp,
-		"--extract-audio",
-		"--audio-format", "mp3",
-		"--audio-quality", "0", // best quality
-		"--output", "%(title)s.%(ext)s",
-		url,
-	)
-
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	if err := cmd.Run(); err != nil {
-		return fmt.Errorf("yt-dlp failed: %w", err)
-	}
-
-	return nil
 }
